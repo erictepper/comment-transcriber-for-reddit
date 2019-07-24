4aaa98a949d8c72a49cc63ca54501724261c40c5
erictepper <eric.tepper@me.com>
2019-07-23 20:32:59 -0700

    Deleted redundant information now available in the new redscriber module.

--- code/main.py
+++ code/main.py
@@ -1,50 +1,6 @@
 import sys
 import praw.exceptions
-import datetime
-
-
-def print_single_comment(comment):
-    print('https://www.reddit.com' + comment.permalink)
-    print()
-    print(comment.author.name + ' ', str(comment.score), 'points ',
-          datetime.datetime.fromtimestamp(comment.created_utc))
-    print(comment.body)
-    print()
-
-
-def print_comment_tree(root_comment, level):  # todo: insert new line breaks when line overflows
-    if level == 0:
-        print('https://www.reddit.com' + root_comment.permalink)
-        print()
-
-    indent_string = indent_level(level)
-
-    try:
-        print(indent_string + root_comment.author.name + ' ', str(root_comment.score), 'points ',
-              datetime.datetime.fromtimestamp(root_comment.created_utc))
-        comment_body_lines = root_comment.body.splitlines()
-        for line in comment_body_lines:
-            print(indent_string + line)
-    except AttributeError:
-        print(indent_string + 'deleted/removed')
-    print(indent_string)
-
-    root_comment.refresh()
-    for reply in root_comment.replies:
-        print_comment_tree(reply, level+1)
-
-
-def print_comment_chain(root_comment, end_comment_id, level):
-    return  # stub
-
-
-def indent_level(level):
-    indent_string = ''
-
-    for i in range(level):
-        indent_string = indent_string + '| '
-
-    return indent_string
+from redscriber import RedditCommentTranscriber
 
 
 if __name__ == '__main__':
@@ -53,22 +9,8 @@ if __name__ == '__main__':
     # PoppinKream: edfm15w
     # Korok seeds: eudig5e
 
-    reddit = praw.Reddit('auth_info')  # auth information stored in git-ignored praw.ini file for confidentiality
-
     start_comment_id = input('ID of start comment: ')
     end_comment_id = input('ID of end comment (or \'all\' to print all children, or \'none\' to print no children): ')
 
-    start_comment = reddit.comment(id=start_comment_id)
-
-    try:
-        start_comment.body
-    except praw.exceptions.ClientException:
-        print('Start comment does not exist.')
-        sys.exit()
-
-    if end_comment_id == 'none' or start_comment_id == end_comment_id:
-        print_single_comment(start_comment)
-    elif end_comment_id == 'all':
-        print_comment_tree(start_comment, 0)
-    else:
-        print_comment_chain(start_comment, end_comment_id, 0)
+    transcriber = RedditCommentTranscriber()
+    transcriber.transcribe(start_comment_id=start_comment_id, end_comment_id=end_comment_id)

70f9fe21cd32933f04d87cae64c243b633a43d89
erictepper <eric.tepper@me.com>
2019-07-24 14:21:59 -0700

    Removed test comments from main class.

--- code/main.py
+++ code/main.py
@@ -2,10 +2,6 @@ from redscriber import RedditCommentTranscriber
 
 
 if __name__ == '__main__':
-    # Comments for testing purposes:
-    # Non-existent: jfkdlszds
-    # PoppinKream: edfm15w
-    # Korok seeds: eudig5e
 
     start_comment_id = input('ID of start comment: ')
     end_comment_id = input('ID of end comment (or \'all\' to print all children, or \'none\' to print no children): ')

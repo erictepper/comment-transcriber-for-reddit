e9afa4ea14977e710f545604c313f1ffbbb4a2e6
erictepper <eric.tepper@me.com>
2019-07-31 22:08:18 -0700

    Edited test cases.

--- code/test.py
+++ code/test.py
@@ -12,16 +12,16 @@ if __name__ == '__main__':
     transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='1')
     transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='jkdsflajfkldsajlfsdka')
 
-
     # Single-comment transcription
     transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='none')
+    transcriber.transcribe(start_comment_id='edfme0h', end_comment_id='none')
 
     # Comment tree transcription
     transcriber.transcribe(start_comment_id='eudig5e', end_comment_id='all')
 
     # Comment chain transcription
     transcriber.transcribe(start_comment_id='eqs3wsz', end_comment_id='eqxjjs8')
-    transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='edfme0h')
+    # transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='edfme0h')
     transcriber.transcribe(start_comment_id='eulkvbp', end_comment_id='eun0q9h')
 
     # Emoji transcription

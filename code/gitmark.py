afec97f0bd8d7b24842d885d735654076362ff27
erictepper <eric.tepper@me.com>
2019-08-11 00:36:09 -0700

    Changed implementation to work as a class with callables rather than a script.

--- code/gitmark.py
+++ code/gitmark.py
@@ -18,7 +18,7 @@ markdown = cmark.cmark_markdown_to_html
 markdown.restype = c_char_p
 markdown.argtypes = [c_char_p, c_long, c_long]
 
-opts = 0 # defaults
+opts = 0  # defaults
 
 
 class CMarkGFM:
@@ -28,13 +28,8 @@ class CMarkGFM:
         if sys.version_info >= (3,0):
             textbytes = text.encode('utf-8')
             textlen = len(textbytes)
-            retval = markdown(textbytes, textlen, opts).decode('utf-8')
-            return cast(retval, c_char_p).value
+            return markdown(textbytes, textlen, opts).decode('utf-8')
         else:
             textbytes = text
             textlen = len(text)
-            retval = markdown(textbytes, textlen, opts)
-            return cast(retval, c_char_p).value
-
-
-sys.stdout.write(CMarkGFM.md2html(sys.stdin.read()))
+            return markdown(textbytes, textlen, opts)

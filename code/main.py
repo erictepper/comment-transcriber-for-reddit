a18ffcdf2e22e87b02ae2dd3d2c56d1c8b2af79b
erictepper <eric.tepper@me.com>
2019-09-04 18:12:26 -0700

    Removed now-unnecessary timing functionality.

--- code/main.py
+++ code/main.py
@@ -1,5 +1,3 @@
-import time
-
 from redscriber import RedditCommentTranscriber
 
 
@@ -8,8 +6,6 @@ if __name__ == '__main__':
     start_comment_id = input('ID of start comment: ')
     end_comment_id = input('ID of end comment (or \'all\' to print all children, or \'none\' to print no children): ')
 
-    start = time.time()
     transcriber = RedditCommentTranscriber()
     transcriber.transcribe(start_comment_id=start_comment_id, end_comment_id=end_comment_id)
-    end = time.time()
     print('Transcribed comment thread in %f s.' % (end-start))

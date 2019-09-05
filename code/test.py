558d121fe06ebf2f97a5ff8daa6df7c9a4482a0a
erictepper <eric.tepper@me.com>
2019-09-04 23:10:24 -0700

    Test cases now run using multiprocessing.

--- code/test.py
+++ code/test.py
@@ -1,4 +1,6 @@
+from multiprocessing import Process
 # import unittest
+
 from redscriber import RedditCommentTranscriber
 
 
@@ -7,39 +9,71 @@ if __name__ == '__main__':
 
     # Comments for testing purposes:
     # Non-existent start comment
-    transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='all')
-    transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='none')
-    transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='1')
-    transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='jkdsflajfkldsajlfsdka')
+    p1 = Process(target=transcriber.transcribe, args=('jfkdlszds', 'all',))
+    p1.start()
+    # transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='all')
+    p2 = Process(target=transcriber.transcribe, args=('jfkdlszds', 'none',))
+    p2.start()
+    # transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='none')
+    p3 = Process(target=transcriber.transcribe, args=('jfkdlszds', '1',))
+    p3.start()
+    # transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='1')
+    p4 = Process(target=transcriber.transcribe, args=('jfkdlszds', 'jkdsflajfkldsajlfsdka',))
+    p4.start()
+    # transcriber.transcribe(start_comment_id='jfkdlszds', end_comment_id='jkdsflajfkldsajlfsdka')
 
     # Single-comment transcription
-    transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='none')
-    transcriber.transcribe(start_comment_id='edfme0h', end_comment_id='none')
+    p5 = Process(target=transcriber.transcribe, args=('edfm15w', 'none',))
+    p5.start()
+    # transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='none')
+    p6 = Process(target=transcriber.transcribe, args=('edfme0h', 'none',))
+    p6.start()
+    # transcriber.transcribe(start_comment_id='edfme0h', end_comment_id='none')
 
     # Comment tree transcription
-    transcriber.transcribe(start_comment_id='eudig5e', end_comment_id='all')
+    p7 = Process(target=transcriber.transcribe, args=('eudig5e', 'all',))
+    p7.start()
+    # transcriber.transcribe(start_comment_id='eudig5e', end_comment_id='all')
 
     # Giant comment tree transcription
-    transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='all')
-    # transcriber.transcribe(start_comment_id='edge98h', end_comment_id='none') - parser has problems with apostrophe
+    p8 = Process(target=transcriber.transcribe, args=('edfm15w', 'all',))
+    p8.start()
+    # transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='all')
+    p9 = Process(target=transcriber.transcribe, args=('edge98h', 'none',))
+    p9.start()
+    # transcriber.transcribe(start_comment_id='edge98h', end_comment_id='none')  # - parser has problems with apostrophe
 
     # Comment chain transcription
-    transcriber.transcribe(start_comment_id='eqs3wsz', end_comment_id='eqxjjs8')
-    transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='edfme0h')
-    transcriber.transcribe(start_comment_id='eulkvbp', end_comment_id='eun0q9h')
+    p10 = Process(target=transcriber.transcribe, args=('eqs3wsz', 'eqxjjs8',))
+    p10.start()
+    # transcriber.transcribe(start_comment_id='eqs3wsz', end_comment_id='eqxjjs8')
+    p11 = Process(target=transcriber.transcribe, args=('edfm15w', 'edfme0h',))
+    p11.start()
+    # transcriber.transcribe(start_comment_id='edfm15w', end_comment_id='edfme0h')
+    p12 = Process(target=transcriber.transcribe, args=('eulkvbp', 'eun0q9h',))
+    p12.start()
+    # transcriber.transcribe(start_comment_id='eulkvbp', end_comment_id='eun0q9h')
 
     # Emoji transcription
     # transcriber.transcribe(start_comment_id='dc2j4tb', end_comment_id='none')
 
     # Table markdown transcription
-    transcriber.transcribe(start_comment_id='ew6ld63', end_comment_id='none')
+    p13 = Process(target=transcriber.transcribe, args=('ew6ld63', 'none',))
+    p13.start()
+    # transcriber.transcribe(start_comment_id='ew6ld63', end_comment_id='none')
 
     # Deleted account transcription
-    transcriber.transcribe(start_comment_id='dc2g3xq', end_comment_id='none')
+    p14 = Process(target=transcriber.transcribe, args=('dc2g3xq', 'none',))
+    p14.start()
+    # transcriber.transcribe(start_comment_id='dc2g3xq', end_comment_id='none')
 
     # Reddit user link & deleted account transcription
-    transcriber.transcribe(start_comment_id='dc20nhd', end_comment_id='all')
+    p15 = Process(target=transcriber.transcribe, args=('dc20nhd', 'all',))
+    p15.start()
+    # transcriber.transcribe(start_comment_id='dc20nhd', end_comment_id='all')
 
     # Subreddit link transcription
-    transcriber.transcribe(start_comment_id='eow0ei7', end_comment_id='none')
+    p7 = Process(target=transcriber.transcribe, args=('eow0ei7', 'none',))
+    p7.start()
+    # transcriber.transcribe(start_comment_id='eow0ei7', end_comment_id='none')
 
